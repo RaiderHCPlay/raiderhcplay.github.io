8938cb4a16a169c12a31515fd1a35676efd5ac36
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-13 21:44:24 +0200

    Update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased bg-blue-950">
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div>
-          <h2 className={"flex justify-center bg-black text-white text-6xl"}>RaiderHCPlay's GitHub IO</h2>
-          <div className={"flex justify-center"}>
-              <Image className={"border rounded-full mt-5"} alt="avatar"
+      <>
+      <h2 className={"flex justify-center bg-black text-white text-6xl"}>RaiderHCPlay's GitHub IO</h2>
+      <div className={"flex justify-center "}>
+          <Image className={"border rounded-full mt-5"} alt="avatar"
                      src="https://avatars.githubusercontent.com/u/170134946?v=4" width="260" height="260"/>
-              <div className={"flex flex-col items-center pl-16 pt-14"}>
-                  <h1 className={"text-orange-400 text-8xl"}>RaiderHCPlay</h1>
-                  <strong><h2 className={"text-5xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span></h2></strong>
-                  <strong className={"mt-5"}><a className={"text-purple-600 text-4xl"} href="https://github.com/RaiderHCPlay"><h2 id="githubLink">GitHub Profile Link</h2></a></strong>
-              </div>
+          <div className={"flex flex-col items-center pl-16 pt-14"}>
+              <h1 className={"text-orange-400 text-8xl"}>RaiderHCPlay</h1>
+              <strong><h2 className={"text-5xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span></h2></strong>
+              <strong className={"mt-5"}><a className={"text-purple-600 text-4xl"} href="https://github.com/RaiderHCPlay"><h2 id="githubLink">GitHub Profile Link</h2></a></strong>
           </div>
       </div>
+      </>
   );
 }
