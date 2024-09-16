324cb4d310b010027e36b5a3a7cdc7debbf27d14
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-16 09:45:04 +0200

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,21 +6,21 @@ export default function Home() {
       <h2 className={"flex justify-center bg-black text-white text-3xl sm:text-xl md:text-2xl lg:text-7xl"}>RaiderHCPlay's GitHub IO</h2>
       <div className={"flex justify-center"}>
           <a href={"https://github.com/RaiderHCPlay"}><Image
-              width="260"
-              height="260"
+              width="100"
+              height="100"
               layout="responsive"
               className="border rounded-full mt-20"
               alt="avatar"
               src="https://avatars.githubusercontent.com/u/170134946?v=4"
           /></a>
           <div className={"flex flex-col items-center pl-16 pt-14"}>
-              <h1 className={"text-orange-400 lg:text-8xl md:text-7xl sm:text-6xl"}>RaiderHCPlay</h1>
-              <strong><h2 className={"lg:text-5xl md:text-4xl sm:text-3xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span>
+              <h1 className={"text-orange-400 lg:text-8xl md:text-7xl sm:text-8xl"}>RaiderHCPlay</h1>
+              <strong><h2 className={"lg:text-5xl md:text-4xl sm:text-5xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span>
               </h2></strong>
-              <strong className={"mt-5"}><a className={"text-purple-600 lg:text-4xl md:text-3xl sm:text-2xl"}
+              <strong className={"mt-5"}><a className={"text-purple-600 lg:text-4xl md:text-3xl sm:text-4xl"}
                                             href="https://github.com/RaiderHCPlay"><h2 id="githubLink">GitHub Profile
                   Link</h2></a></strong>
-              <strong className={"mt-5"}><a className={"text-green-600 lg:text-4xl md:text-3xl sm:text-2xl"}
+              <strong className={"mt-5"}><a className={"text-green-600 lg:text-4xl md:text-3xl sm:text-4xl"}
                                             href="https://github.com/RaiderHCPlay/raiderhcplay.github.io">
                   <h2 id="githubLink">Page Source Code</h2></a></strong>
           </div>
