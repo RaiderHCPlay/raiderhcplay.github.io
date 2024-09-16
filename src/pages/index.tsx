4eeef4de4a7ca2dcab67022c24493b96379e047a
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-16 09:29:46 +0200

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
       <>
       <h2 className={"flex justify-center bg-black text-white text-3xl sm:text-xl md:text-2xl lg:text-7xl"}>RaiderHCPlay's GitHub IO</h2>
       <div className={"flex justify-center"}>
-          <a href={"https://github.com/RaiderHCPlay"}><img className={"border rounded-full mt-20 h-auto max-w-full"} alt="avatar"
-                    src="https://avatars.githubusercontent.com/u/170134946?v=4"/></a>
+          <div className={"border rounded-full mt-20 h-auto max-w-full"}>
+              <a href={"https://github.com/RaiderHCPlay"}><Image width="260" height="260"
+                                                                 className={"rounded-full"}
+                                                                 alt="avatar"
+                                                                 src="https://avatars.githubusercontent.com/u/170134946?v=4"/></a>
+          </div>
           <div className={"flex flex-col items-center pl-16 pt-14"}>
               <h1 className={"text-orange-400 lg:text-8xl md:text-7xl sm:text-6xl"}>RaiderHCPlay</h1>
               <strong><h2 className={"lg:text-5xl md:text-4xl sm:text-3xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span>
