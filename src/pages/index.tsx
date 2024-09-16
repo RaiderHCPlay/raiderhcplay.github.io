4d504974a18ae615a5e4b683d68c8ee4db7835f3
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-16 10:23:04 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,7 @@ export default function Home() {
       <>
       <h2 className={"flex justify-center bg-black text-white text-3xl sm:text-xl md:text-2xl lg:text-7xl"}>RaiderHCPlay's GitHub IO</h2>
       <div className={"flex justify-center"}>
-          {/*<a href={"https://github.com/RaiderHCPlay"}><Image*/}
-          {/*    width="100"*/}
-          {/*    height="100"*/}
-          {/*    layout="responsive"*/}
-          {/*    className="border rounded-full mt-20"*/}
-          {/*    alt="avatar"*/}
-          {/*    src="https://avatars.githubusercontent.com/u/170134946?v=4"*/}
-          {/*/></a>*/}
-          <div className={"flex flex-col items-center pl-16 pt-14 w-auto h-auto"}>
+          <div className={"flex flex-col items-center pl-16 pt-14"}>
               <h1 className={"text-orange-400 text-8xl sm:text-xl md:text-xl lg:text-8xl"}>RaiderHCPlay</h1>
               <strong><h2 className={"text-5xl sm:text-xl md:text-2xl lg:text-5xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span>
               </h2></strong>
