20bc298c297ba970f46945e749c422393ceb7459
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-16 09:49:39 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,13 @@ export default function Home() {
           {/*    src="https://avatars.githubusercontent.com/u/170134946?v=4"*/}
           {/*/></a>*/}
           <div className={"flex flex-col items-center pl-16 pt-14"}>
-              <h1 className={"text-orange-400 lg:text-8xl md:text-7xl sm:text-8xl"}>RaiderHCPlay</h1>
-              <strong><h2 className={"lg:text-5xl md:text-4xl sm:text-5xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span>
+              <h1 className={"text-orange-400 text-8xl lg:text-8xl md:text-7xl sm:text-8xl"}>RaiderHCPlay</h1>
+              <strong><h2 className={"lg:text-5xl text-5xl md:text-4xl sm:text-5xl pt-4 text-typescript"}><span>TypeScript </span><span>Developer</span>
               </h2></strong>
-              <strong className={"mt-5"}><a className={"text-purple-600 lg:text-4xl md:text-3xl sm:text-4xl"}
+              <strong className={"mt-5"}><a className={"text-purple-600 text-4xl lg:text-4xl md:text-3xl sm:text-4xl"}
                                             href="https://github.com/RaiderHCPlay"><h2 id="githubLink">GitHub Profile
                   Link</h2></a></strong>
-              <strong className={"mt-5"}><a className={"text-green-600 lg:text-4xl md:text-3xl sm:text-4xl"}
+              <strong className={"mt-5"}><a className={"text-green-600 text-4xl lg:text-4xl md:text-3xl sm:text-4xl"}
                                             href="https://github.com/RaiderHCPlay/raiderhcplay.github.io">
                   <h2 id="githubLink">Page Source Code</h2></a></strong>
           </div>
