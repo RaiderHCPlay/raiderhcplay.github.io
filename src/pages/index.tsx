de6ed2f9cee89617de9f87b61cf9b4394439f0d1
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-17 08:51:28 +0200

    Updated links and added license link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,27 +23,34 @@ export default function Home() {
                           Paweł Fabrycki
                       </h1>
                   </strong>
-                  <h2 className={"text-orange-400 text-xl sm:text-xl md:text-2xl lg:text-3xl mt-2"}>(also known as <strong>RaiderHCPlay</strong>)</h2>
+                  <h2 className={"text-orange-400 text-xl sm:text-xl md:text-2xl lg:text-3xl mt-2"}>(also known
+                      as <strong>RaiderHCPlay</strong>)</h2>
                   <strong className={"mt-2"}>
                       <h2 className={"text-2xl sm:text-2xl md:text-4xl lg:text-7xl text-typescript"}>
                           <span>TypeScript </span><span>Programmer</span>
                       </h2>
                   </strong>
                   <strong className={"mt-5"}>
-                      <a className={"text-purple-600 text-2xl sm:text-2xl md:text-4xl lg:text-4xl"}
+                      <a className={"text-2xl sm:text-2xl md:text-4xl lg:text-4xl text-decoration-line: underline visited:text-purple-600 hover:text-orange-400 active:text-blue-600"}
                          href="https://github.com/RaiderHCPlay"
                          target={"_blank"}>
                           <h2 id="githubLink">GitHub Profile Link</h2>
                       </a>
                   </strong>
                   <strong className={"mt-3"}>
-                      <a className={"text-green-600 text-2xl sm:text-2xl md:text-4xl lg:text-4xl"}
+                      <a className={"visited:text-green-600 text-2xl sm:text-2xl md:text-4xl lg:text-4xl text-decoration-line: underline hover:text-orange-400 active:text-blue-600"}
                          href="https://github.com/RaiderHCPlay/raiderhcplay.github.io"
                          target={"_blank"}>
                           <h2 id="githubLink">Page Source Code</h2>
                       </a>
                   </strong>
               </div>
+          </div>
+          <div className={"flex justify-center absolute bottom-0 left-0 right-0 p-4"}>
+              <h2 className={"text-xl sm:text-xl md:text-2xl lg:text-3xl"}>This page is under <a
+                      className={"text-decoration-line: underline visited:text-red-600 hover:text-orange-400 active:text-blue-600"}
+                     href={"https://www.gnu.org/licenses/gpl-3.0.html"} target={"_blank"}>GPL-3.0
+                  License</a></h2>
           </div>
 
       </>
