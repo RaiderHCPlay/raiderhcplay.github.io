f6896cb385da596b0a8cc694b203f99a5543f28b
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-17 09:01:31 +0200

    Updated links colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,14 +31,14 @@ export default function Home() {
                       </h2>
                   </strong>
                   <strong className={"mt-5"}>
-                      <a className={"text-2xl sm:text-2xl md:text-4xl lg:text-4xl text-decoration-line: underline visited:text-purple-600 hover:text-orange-400 active:text-blue-600"}
+                      <a className={"text-purple-600 text-2xl sm:text-2xl md:text-4xl lg:text-4xl text-decoration-line: underline visited:text-purple-600 hover:text-orange-400 active:text-blue-600"}
                          href="https://github.com/RaiderHCPlay"
                          target={"_blank"}>
                           <h2 id="githubLink">GitHub Profile Link</h2>
                       </a>
                   </strong>
                   <strong className={"mt-3"}>
-                      <a className={"visited:text-green-600 text-2xl sm:text-2xl md:text-4xl lg:text-4xl text-decoration-line: underline hover:text-orange-400 active:text-blue-600"}
+                      <a className={"text-green-600 visited:text-green-600 text-2xl sm:text-2xl md:text-4xl lg:text-4xl text-decoration-line: underline hover:text-orange-400 active:text-blue-600"}
                          href="https://github.com/RaiderHCPlay/raiderhcplay.github.io"
                          target={"_blank"}>
                           <h2 id="githubLink">Page Source Code</h2>
@@ -48,7 +48,7 @@ export default function Home() {
           </div>
           <div className={"flex justify-center absolute bottom-0 left-0 right-0 p-4"}>
               <h2 className={"text-xl sm:text-xl md:text-2xl lg:text-3xl"}>This page is under <a
-                  className={"text-decoration-line: underline visited:text-red-600 hover:text-orange-400 active:text-blue-600"}
+                  className={"text-decoration-line: underline text-red-600 visited:text-red-600 hover:text-orange-400 active:text-blue-600"}
                   href={"https://www.gnu.org/licenses/gpl-3.0.html"} target={"_blank"}>GPL-3.0 License</a></h2>
           </div>
 
