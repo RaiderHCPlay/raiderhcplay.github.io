92720d9eb576b2531aa46f897d01a87e9b343c22
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-18 09:46:25 +0200

    Added links to frameworks logos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
                   Fullstack Developer with one year of experience. Currently Student!
                   I mainly write in <span className={"text-typescript"}>TypeScript</span>.
                   <dl>Learning frameworks:
-                      <li>React <svg width="100%" height="100%" viewBox="-10.5 -9.45 21 18.9" fill="none"
+                      <li>React <a href={"https://react.dev/"} target={"_blank"}><svg width="100%" height="100%" viewBox="-10.5 -9.45 21 18.9" fill="none"
                                      xmlns="http://www.w3.org/2000/svg"
                                      className="inline w-4 h-4 sm:w-4 sm:h-4 md:w-8 md:h-8 lg:w-14 lg:h-14 text-sm me-0 text-brand dark:text-brand-dark origin-center transition-all ease-in-out text-react">
                           <circle cx="0" cy="0" r="2" fill="currentColor"></circle>
@@ -61,22 +61,22 @@ export default function Home() {
                               <ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse>
                               <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse>
                           </g>
-                      </svg></li>
-                      <li>NextJS <Image
-                            alt={"NextJS Logo"}
+                      </svg></a></li>
+                      <li>Next.js <a href={"https://nextjs.org/"} target={"_blank"}><Image
+                            alt={"Next.js Logo"}
                             width={"50"}
                             height={"50"}
                             className={"inline w-4 h-4 sm:w-4 sm:h-4 md:w-8 md:h-8 lg:w-14 lg:h-14"}
                             src={"https://camo.githubusercontent.com/c3635f27439ecdbf20e3cbf969c156f4040f10a0c8c836cf307d916dd8f806d4/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313636323133303535392f6e6578746a732f49636f6e5f6461726b5f6261636b67726f756e642e706e67"}
-                      />
+                      /></a>
                       </li>
-                      <li>Electron <Image
+                      <li>Electron <a href={"https://www.electronjs.org/"} target={"_blank"}><Image
                             className={"inline w-4 h-4 sm:w-4 sm:h-4 md:w-8 md:h-8 lg:w-14 lg:h-14"}
                             alt={"Electron Logo"}
                             width={"50"}
                             height={"50"}
                             src={"https://www.electronjs.org/assets/img/logo.svg"}
-                      /></li>
+                      /></a></li>
                   </dl>
               </article>
           </div>
