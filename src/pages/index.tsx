19abbcee329723302a82d77f76c7c5425eed183b
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-18 19:40:48 +0200

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,7 +98,7 @@ export default function Home() {
           </div>
           <div className={"flex justify-center bottom-0 left-0 right-0 p-4 relative text-white"}>
               <h2 className={"text-xl sm:text-xl md:text-2xl lg:text-3xl"}>This page is under <a
-                  className={"text-decoration-line: underline text-red-600 visited:text-red-600 hover:text-orange-400 active:text-blue-600"}
+                  className={"text-decoration-line: underline text-red-600 visited:text-red-600 hover:text-blue-400 active:text-purple-600"}
                   href={"https://www.gnu.org/licenses/gpl-3.0.html"} target={"_blank"}><strong>GPL-3.0 License</strong></a></h2>
           </div>
           </div>
