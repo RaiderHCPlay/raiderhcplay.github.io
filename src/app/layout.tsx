854b003ca912dd1472c569bfb8d65aaaf9d7dade
RaiderHCPlay <fabryckipawel@gmail.com>
2024-09-29 20:47:07 +0200

    Added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "RaiderHCPlay's website",
   description: "RaiderHCPlay's personal website",
+  icons: "https://avatars.githubusercontent.com/u/170134946?v=4"
 };
 
 export default function RootLayout({
