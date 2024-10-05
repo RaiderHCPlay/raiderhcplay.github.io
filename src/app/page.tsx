13ccb6b45c3f7baa06111cfe92892e03487a418d
RaiderHCPlay <fabryckipawel@gmail.com>
2024-10-05 19:02:57 +0200

    Updated for smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from 'next/link';
 
 export default function Page() {
     return (
@@ -12,7 +12,7 @@ export default function Page() {
                     className="border rounded-full ml-[20px] lg:ml-[80px] sm:w-8 sm:h-8 md:w-16 md:h-16 lg:w-28 lg:h-28 mt-2 mb-2"
                     alt="avatar"
                     src="https://avatars.githubusercontent.com/u/170134946?v=4"/>
-                    <h1 className={"text-[20px] md:text-[32px] lg:text-[64px] lg:pl-12 pl-4"}>RaiderHCPlay</h1>
+                    <Link href="/" className={"text-[20px] md:text-[32px] lg:text-[64px] lg:pl-12 pl-4"}>RaiderHCPlay</Link>
                     <a href="#aboutMe" className={"text-[15px] md:text-[24px] lg:text-[50px] lg:pl-[150px] pl-[6px]"}>About</a>
                 </div>
             </header>
@@ -56,7 +56,7 @@ export default function Page() {
                     </div>
                 <div id="aboutMe" className={"flex flex-col items-center pt-14 text-white "}>
                     <div className={"text-center"}>
-                        <h1 className={"text-2xl sm:text-2xl md:text-4xl lg:text-8xl pb-1.5"}>About me</h1>
+                        <h1 className={"text-3xl md:text-4xl lg:text-8xl pb-1.5"}>About me</h1>
                         <article className={"text-2xl sm:text-2xl  md:text-3xl lg:text-4xl max-w-7xl"}>Hi!
                             My name is Paweł Fabrycki. I'm a
                             Programmer with one year of experience. Currently Student!
