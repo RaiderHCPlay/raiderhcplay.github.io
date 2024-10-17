f49a06d5fb216115665596644c33a5be7144da78
RaiderHCPlay <fabryckipawel@gmail.com>
2024-10-17 22:06:29 +0200

    Updated for smaller screens

--- src/app/Introduction.tsx
+++ src/app/Introduction.tsx
@@ -13,7 +13,7 @@ export default function Introduction() {
                 src="https://avatars.githubusercontent.com/u/170134946?v=4"
             />
         </a>
-        <div className={"flex flex-col items-center ml-14 pt-12"}>
+        <div className={"flex flex-col items-center ml-4 md:ml-14 pt-12"}>
         <strong>
             <h1 className={"text-orange-600 text-[25px] md:text-8xl"}>
                 Paweł Fabrycki
@@ -22,12 +22,12 @@ export default function Introduction() {
         <h2 className={"text-orange-600 text-[15px] md:text-3xl mt-2"}>(also known
             as <strong>RaiderHCPlay</strong>)</h2>
         <strong className={"mt-2"}>
-            <h2 className={"text-[20px] md:text-7xl text-typescript"}>
+            <h2 className={"text-[18px] md:text-7xl text-typescript"}>
                 TypeScript Programmer
             </h2>
         </strong>
         <strong className={"mt-5"}>
-            <a className={"text-white text-[20px] md:text-4xl text-decoration-line: underline visited:text-white hover:text-blue-400 active:text-purple-600"}
+            <a className={"text-white text-[18px] md:text-4xl text-decoration-line: underline visited:text-white hover:text-blue-400 active:text-purple-600"}
                href="https://github.com/RaiderHCPlay"
                target={"_blank"}>
                 <h2 id="githubLink">GitHub Profile Link</h2>
@@ -37,12 +37,12 @@ export default function Introduction() {
             <Link
                 href={"https://wakatime.com/@RaiderHCPlay"}
                 target={"_blank"}
-                className={"text-white text-[20px] md:text-4xl text-decoration-line: underline visited:text-white hover:text-blue-400 active:text-purple-600"}
+                className={"text-white text-[18px] md:text-4xl text-decoration-line: underline visited:text-white hover:text-blue-400 active:text-purple-600"}
             >WakaTime Profile Link
             </Link>
         </b>
         <b className={"mt-3"}>
-            <a className={"text-white text-[20px] md:text-4xl text-decoration-line: underline visited:text-white hover:text-blue-400 active:text-purple-600"}
+            <a className={"text-white text-[18px] md:text-4xl text-decoration-line: underline visited:text-white hover:text-blue-400 active:text-purple-600"}
                href="https://github.com/RaiderHCPlay/raiderhcplay.github.io"
                target={"_blank"}>
                 <h2 id="githubLink">Page Source Code</h2>
