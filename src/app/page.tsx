52663ab89f49d81158fba2c7802141489bfc2d7c
RaiderHCPlay <fabryckipawel@gmail.com>
2024-10-18 20:56:09 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Header from "@/app/Header";
 import Footer from '@/app/Footer';
 import Introduction from "@/app/Introduction";
 import About from '@/app/About';
+import Projects from "@/app/Projects";
 
 export default function Page() {
 
@@ -20,10 +21,11 @@ export default function Page() {
             <Header optionsAnimation={optionsAnimation} onAnimationStart={settingMobileOptions} onClick={() => {
                 setOptionsAnimation(true)
             }} onAnimationEnd={() => setOptionsAnimation(false)} mobileOptions={mobileOptions}/>
-            <div className="bg-gradient-to-b from-react to-pink-400">
+            <div className="bg-gradient-to-b from-react to-purple-400">
                 <Introduction/>
                 <About/>
                 <Footer/>
+                <Projects/>
             </div>
 
         </>
