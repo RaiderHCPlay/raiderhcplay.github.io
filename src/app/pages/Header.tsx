c93e743046b8598427ba58730109bf57bbc3d832
RaiderHCPlay <fabryckipawel@gmail.com>
2024-12-23 23:30:52 +0100

    feat(Header): Added Image for header

--- src/app/pages/Header.tsx
+++ src/app/pages/Header.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Header(props: {
     optionsAnimation: boolean,
     onAnimationStart: () => void,
@@ -5,7 +7,16 @@ export default function Header(props: {
     onAnimationEnd: () => void,
     mobileOptions: boolean
 }) {
-    return <header className={"bg-[#11111b]"}>
+    return <header className={"bg-[#11111b] w-full h-[65px] flex"}>
+        <div className={"flex flex-row justify-center items-center"}>
+            <Image
+                src={"https://avatars.githubusercontent.com/u/170134946?v=4"}
+                alt={"Avatar"}
+                width={"30"}
+                height={"30"}
+                className={"rounded-full md:w-[60px] md:h-[60px]"}
+            />
+        </div>
         <div
             className={"flex justify-end lg:hidden text-[20px] animate-fade-left animate-duration-1500 animate-delay-200"}>
             <button className={`${props.optionsAnimation && "animate-spin animate-once animate-duration-300"
