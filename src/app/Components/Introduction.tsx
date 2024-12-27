7a2adef932ba63055dd6e4be96c3e53c0c1fca86
RaiderHCPlay <fabryckipawel@gmail.com>
2024-12-27 21:17:51 +0100

    chore(Intro): Changed size for mobile

--- src/app/Components/Introduction.tsx
+++ src/app/Components/Introduction.tsx
@@ -14,21 +14,21 @@ export default function Introduction() {
                 src="https://avatars.githubusercontent.com/u/170134946?v=4"
             />
         </a>
-        <div className={"flex flex-col items-center md:ml-14 pt-12"}>
+        <div className={"flex flex-col items-center md:ml-14 pt-2 md:pt-12"}>
         <strong>
-            <h1 className={"text-orange-600 text-[20px] md:text-5xl"}>
+            <h1 className={"text-orange-600 text-[30px] md:text-5xl"}>
                 Paweł Fabrycki
             </h1>
         </strong>
-        <h2 className={"text-orange-600 text-[10px] md:text-2xl mt-2"}>(also known
+        <h2 className={"text-orange-600 text-[20px] md:text-2xl mt-2"}>(also known
             as <strong>RaiderHCPlay</strong>)</h2>
         <strong className={"mt-2"}>
-            <h2 className={"text-[13px] md:text-4xl text-typescript"}>
+            <h2 className={"text-[25px] md:text-4xl text-typescript"}>
                 TypeScript Programmer
             </h2>
         </strong>
         <strong className={"mt-2 md:mt-5"}>
-            <a className={"text-[13px] md:text-3xl text-decoration-line: underline hover:text-[#89b4fa] active:text-purple-600"}
+            <a className={"text-[20px] md:text-3xl text-decoration-line: underline hover:text-[#89b4fa] active:text-purple-600"}
                href="https://github.com/RaiderHCPlay"
                target={"_blank"}>GitHub Profile
             </a>
@@ -37,7 +37,7 @@ export default function Introduction() {
             <Link
                 href={"https://wakatime.com/@RaiderHCPlay"}
                 target={"_blank"}
-                className={"text-[13px] md:text-3xl text-decoration-line: underline hover:text-[#89b4fa] active:text-purple-600"}
+                className={"text-[20px] md:text-3xl text-decoration-line: underline hover:text-[#89b4fa] active:text-purple-600"}
             >WakaTime Profile
             </Link>
         </b>
