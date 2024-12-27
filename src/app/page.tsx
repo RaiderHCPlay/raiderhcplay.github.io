3ee29f4262b93ac7e506bacdf9d79faa2b3d7304
RaiderHCPlay <fabryckipawel@gmail.com>
2024-12-27 11:48:59 +0100

    chore(dir): renamed directory from pages to Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import {useState} from 'react';
-import Header from "@/app/pages/Header";
-import Footer from '@/app/pages/Footer';
-import Introduction from "@/app/pages/Introduction";
-import About from '@/app/pages/About';
-import Projects from "@/app/pages/Projects";
+import Header from "@/app/Components/Header";
+import Footer from '@/app/Components/Footer';
+import Introduction from "@/app/Components/Introduction";
+import About from '@/app/Components/About';
+import Projects from "@/app/Components/Projects";
 
 export default function Page() {
 
