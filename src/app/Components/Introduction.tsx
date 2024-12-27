ceb67757a217191f2e401867d28f2073e7321782
RaiderHCPlay <fabryckipawel@gmail.com>
2024-12-27 21:58:04 +0100

    chore(Intro): updated to green

--- src/app/Components/Introduction.tsx
+++ src/app/Components/Introduction.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Introduction() {
     return (
         <div
-            className={"flex-col md:flex-row flex justify-center items-center animate-fade-down animate-duration-[2000ms] w-[325px] md:w-[750px] m-auto border-[#94e2d5] border-[3px] mt-2.5 p-3.5"}>
+            className={"flex-col md:flex-row flex justify-center items-center animate-fade-down animate-duration-[2000ms] w-[325px] md:w-[750px] m-auto border-[#a6e3a1] border-[3px] mt-2.5 p-3.5"}>
         <a href={"https://github.com/RaiderHCPlay"} target={"_blank"}>
             <Image
                 width="110"
@@ -15,32 +15,33 @@ export default function Introduction() {
             />
         </a>
         <div className={"flex flex-col items-center md:ml-14"}>
-        <strong>
-            <h1 className={"text-orange-600 text-[30px] md:text-5xl"}>
-                Paweł Fabrycki
-            </h1>
-        </strong>
-        <h2 className={"text-orange-600 text-[20px] md:text-2xl mt-2"}>(also known
-            as <strong>RaiderHCPlay</strong>)</h2>
-        <strong className={"mt-2"}>
-            <h2 className={"text-[25px] md:text-4xl text-typescript"}>
-                TypeScript Programmer
+            <strong>
+                <h1 className={"text-[#a6e3a1] text-[30px] md:text-5xl"}>
+                    Paweł Fabrycki
+                </h1>
+            </strong>
+            <h2 className={"text-[#a6e3a1] text-[20px] md:text-2xl mt-2"}>(also known
+                as <strong>RaiderHCPlay</strong>)
             </h2>
-        </strong>
-        <strong className={"mt-2 md:mt-5"}>
-            <a className={"text-[20px] md:text-3xl text-decoration-line: underline hover:text-[#89b4fa] active:text-purple-600"}
-               href="https://github.com/RaiderHCPlay"
-               target={"_blank"}>GitHub Profile
-            </a>
-        </strong>
-        <b className={"mt-1 md:mt-3"}>
-            <Link
-                href={"https://wakatime.com/@RaiderHCPlay"}
-                target={"_blank"}
-                className={"text-[20px] md:text-3xl text-decoration-line: underline hover:text-[#89b4fa] active:text-purple-600"}
-            >WakaTime Profile
-            </Link>
-        </b>
+            <strong className={"mt-2"}>
+                <h2 className={"text-[25px] md:text-4xl text-typescript"}>
+                    TypeScript Programmer
+                </h2>
+            </strong>
+            <strong className={"mt-2 md:mt-5"}>
+                <a className={"text-[20px] md:text-3xl text-decoration-line: underline hover:text-[#a6e3a1] active:text-purple-600"}
+                   href="https://github.com/RaiderHCPlay"
+                   target={"_blank"}>GitHub Profile
+                </a>
+            </strong>
+            <b className={"mt-1 md:mt-3"}>
+                <Link
+                    href={"https://wakatime.com/@RaiderHCPlay"}
+                    target={"_blank"}
+                    className={"text-[20px] md:text-3xl text-decoration-line: underline hover:text-[#a6e3a1] active:text-purple-600"}
+                >WakaTime Profile
+                </Link>
+            </b>
         </div>
     </div>
 )
