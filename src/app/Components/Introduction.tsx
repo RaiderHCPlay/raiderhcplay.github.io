92d14875dcac1bc28f3379651c5a58e143867960
RaiderHCPlay <fabryckipawel@gmail.com>
2024-12-27 21:33:39 +0100

    experiment(Intro): experimenting with border

--- src/app/Components/Introduction.tsx
+++ src/app/Components/Introduction.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 export default function Introduction() {
     return (
         <div
-            className={"flex-col md:flex-row flex justify-center items-center animate-fade-down animate-duration-[2000ms]"}>
+            className={"flex-col md:flex-row flex justify-center items-center animate-fade-down animate-duration-[2000ms] w-1/2 m-auto border-[#94e2d5] border-[3px] mt-2.5"}>
         <a href={"https://github.com/RaiderHCPlay"} target={"_blank"}>
             <Image
                 width="120"
                 height="120"
-                className="border rounded-full mt-10 md:w-60 md:h-60"
+                className="border rounded-full md:w-60 md:h-60"
                 alt="avatar"
                 src="https://avatars.githubusercontent.com/u/170134946?v=4"
             />
         </a>
-        <div className={"flex flex-col items-center md:ml-14 pt-2 md:pt-12"}>
+        <div className={"flex flex-col items-center md:ml-14 pt-2"}>
         <strong>
             <h1 className={"text-orange-600 text-[30px] md:text-5xl"}>
                 Paweł Fabrycki
