16a7a3c7cc2c81ad12a61c95d0a00ab325a88988
RaiderHCPlay <fabryckipawel@gmail.com>
2025-01-01 00:19:21 +0100

    chore(notFound): updated not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,8 +7,8 @@ export default function NotFound() {
         <>
         <Header />
         <div className={"flex flex-col items-center justify-center pt-2.5 animate-fade-down animate-delay-200 animate-duration-1500"}>
-            <h1 className={"text-[50px] md:text-5xl"}>Page you are looking for is for now unavailable</h1>
-            <Link href={"/"}><button className={"underline active:text-[#cba6f7] hover:text-[#a6e3a1]  text-[45px] md:text-4xl pt-2.5"}>Back to main page</button></Link>
+            <h1 className={"text-[40px] md:text-[50px] md:text-5xl"}>Page unavailable</h1>
+            <Link href={"/"}><button className={"underline active:text-[#cba6f7] hover:text-[#a6e3a1]  text-[40px] md:text-4xl pt-2.5"}>Back to main page</button></Link>
         </div>
             <Footer />
         </>
