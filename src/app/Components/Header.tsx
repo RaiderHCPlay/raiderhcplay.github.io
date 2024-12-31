f4192b081eec4ebecfa721f59edc7070995d4500
RaiderHCPlay <fabryckipawel@gmail.com>
2024-12-31 21:53:52 +0100

    chore(Header): edited animations

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -5,10 +5,7 @@ import {useState} from "react";
 export default function Header() {
     const [mobileOptions, setMobileOptions] = useState(false);
     const [optionsAnimation, setOptionsAnimation] = useState(false);
-
-    const settingMobileOptions = () => {
-        setMobileOptions(!mobileOptions);
-    }
+    const [spinning, setSpinning] = useState(false);
 
     return <header className={"bg-[#11111b] w-full h-[35px] md:h-[65px] flex items-center justify-center animate-fade-down animate-delay-200 animate-duration-1500"}>
         <div className={"flex flex-row justify-center items-center"}>
@@ -23,15 +20,22 @@ export default function Header() {
             </a>
         </div>
         <div
-            className={"flex justify-end md:hidden text-[20px] animate-fade-left animate-duration-1500 animate-delay-200"}>
-            <button className={`${optionsAnimation && "animate-spin animate-once animate-duration-300"
+            className={"flex justify-end  text-[20px] animate-fade-left animate-duration-1500 animate-delay-200"}>
+            <button className={`${optionsAnimation ? (mobileOptions ? "animate-spin animate-once animate-duration-500" : "animate-spin animate-once animate-duration-200") : ""
             }`}
-                    onAnimationStart={settingMobileOptions}
+                    onAnimationStart={() => {
+                        setMobileOptions(!mobileOptions);
+                    }
+            }
                     onClick={() => {
+                        setSpinning(!spinning);
                         setOptionsAnimation(true)
                     }}
-                    onAnimationEnd={() => setOptionsAnimation(false)}>{!mobileOptions ? "=" : "x"}</button>
-            <div className={mobileOptions ? "animate-fade-left" : "hidden"}>
+                    onAnimationEnd={() => {
+                        setOptionsAnimation(false);
+                    }
+            }>{!mobileOptions ? "=" : "x"}</button>
+            <div className={spinning ? "animate-fade-left animate-duration-500" : "hidden"}>
                 <h1>In dev</h1>
             </div>
         </div>
