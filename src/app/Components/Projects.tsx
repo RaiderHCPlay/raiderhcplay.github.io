693ff6877dfa31eec3473b9ab359b5be411a404d
RaiderHCPlay <fabryckipawel@gmail.com>
2025-01-04 12:25:27 +0100

    chore: Updated some style and started adding projects

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -2,6 +2,12 @@ export default function Projects() {
     return(
         <div className={"container"}>
             <h1 className={"projectsTitle"}>Projects</h1>
+            <div className={"projects"}>
+                <div className={"project"}>
+                    <h1 className={"projectTitle"}>raiderhcplay.github.io</h1>
+                    <desc className={"projectDescription"}>My website and webdev sandbox</desc>
+                </div>
+            </div>
         </div>
         )
 }
