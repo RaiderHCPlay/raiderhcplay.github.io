9a433e9168ec737e069dce6d9b2daeb647148af8
RaiderHCPlay <fabryckipawel@gmail.com>
2025-01-05 14:34:27 +0100

    temp: removed first project

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -3,10 +3,10 @@ export default function Projects() {
         <div className={"container"}>
             <h1 className={"projectsTitle"}>Projects</h1>
             <div className={"projects"}>
-                <div className={"project"}>
-                    <h1 className={"projectTitle"}>raiderhcplay.github.io</h1>
-                    <desc className={"projectDescription"}>My website and webdev sandbox</desc>
-                </div>
+                {/*<div className={"project"}>*/}
+                {/*    <h1 className={"projectTitle"}>raiderhcplay.github.io</h1>*/}
+                {/*    <desc className={"projectDescription"}>My website and webdev sandbox</desc>*/}
+                {/*</div>*/}
             </div>
         </div>
         )
